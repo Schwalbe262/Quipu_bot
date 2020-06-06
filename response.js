ae33545126c2fc873f8640ab0386fda1b00d8801
Schwalbe262 <r1jae@naver.com>
2020-06-07 01:41:33 +0900

    Update response.js

--- response.js
+++ response.js
@@ -117,10 +117,16 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
                     java.lang.Thread.sleep(300)
                 }
                 if( comm_body[i].split("$$$")[3] == "identity_multy" ){
-                    let nickName = String(comm_body[i].split("$$$")[4]).split("$%$")[0]
-                    let id = String(comm_body[i].split("$$$")[4]).split("$%$")[1]
-                    let body = "$$$"+(i+1)+"$$$"+comm_body[i].split("$$$")[2]+"$$$"+"return_identity_multi"+"$$$"+nickName+"$%$"+NSC1(id)
-                    Api.replyRoom("통신방",body)
+                    try{
+                        let nickName = String(comm_body[i].split("$$$")[4]).split("$%$")[0]
+                        let id = String(comm_body[i].split("$$$")[4]).split("$%$")[1]
+                        let body = "$$$"+(i+1)+"$$$"+comm_body[i].split("$$$")[2]+"$$$"+"return_identity_multi"+"$$$"+nickName+"$%$"+NSC1(id)
+                        Api.replyRoom("통신방",body)
+                    }
+                    catch(e){
+                        Api.replyRoom("봇강의방", "Response Error\n" + e + "\n" + e.stack + "\n" + e.rhinoException);
+                    }
+
                 }
 
 
