fce17d8b0dc9b0719cf4bad75f97c0c3ac63e5dd
Schwalbe262 <r1jae@naver.com>
2020-06-07 21:26:56 +0900

    Update response.js

--- response.js
+++ response.js
@@ -135,7 +135,6 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
                         java.lang.Thread.sleep(500)
                     }
                     if( comm_body[i].split("$$$")[3] == "identity_multy_v3" ){
-                        Api.replyRoom("통신방","웅앙완료")
                         let nickName = String(comm_body[i].split("$$$")[4]).split("$%$")[0]
                         let id = String(comm_body[i].split("$$$")[4]).split("$%$")[1]
                         let body = "$$$"+(i+1)+"$$$"+comm_body[i].split("$$$")[2]+"$$$"+"return_identity_multi_v3"+"$$$"+nickName+"$%$"+NSC1(id)
