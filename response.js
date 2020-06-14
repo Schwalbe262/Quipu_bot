d5cd7cc82efa916460119a65d3a84ff303518b52
Schwalbe262 <r1jae@naver.com>
2020-06-14 14:27:26 +0900

    Update response.js

--- response.js
+++ response.js
@@ -15,6 +15,15 @@ function require(src,force){
     }
 }
 
+//================= 통신 변수 ===================
+var comm_flag = 0
+var comm_body = []
+var comm_end_flag = 0
+var num_body
+var comm_place
+
+Log.d("1")
+
 const D=require("DBManager.js")
 
 var PM=android.os.PowerManager;
@@ -4510,6 +4519,76 @@ function response(room, msg, sender, isGroupChat, replier, imageDB) {
             Api.replyRoom("시갤톡방",temp_Ham)
         }
 
+        //==============================================================================================================
+        //================================================== 통신 ======================================================
+
+        // body packet의 정의 : $$$1$$$cat$$$c=2&&c=4
+
+        if( room=="통신방" && msg==("$$$$$start$$$$$") && sender!="시립봇(sub"){ // 통신 시작 코드
+            comm_flag = 1
+            comm_body = [];
+        }
+        else if( room=="통신방" && comm_flag == 1 && msg.indexOf("$$$")==0 && msg!="$$$$$end$$$$$"){ // 통신 내용 수신
+            comm_body[Number(msg.split("$$$")[1])-1] = msg
+        }
+        else if( room=="통신방" && msg=="$$$$$end$$$$$" ){ // 통신 종료 코드
+            comm_flag = 0
+            comm_end_flag = 1
+            num_body = comm_body.length
+        }
+        //냠
+
+        //================================================== 통신 끝====================================================
+        //==============================================================================================================
+
+        if( comm_end_flag==1 && num_body>0 ){
+            comm_end_flag=0
+            Api.replyRoom("통신방","$$$$$start$$$$$")
+            for(let i=0;i<num_body;i++){
+
+                try{
+                    if( comm_body[i].split("$$$")[3] == "identity_single" ){
+                        let id = String(comm_body[i].split("$$$")[4])
+                        let body = "$$$"+(i+1)+"$$$"+comm_body[i].split("$$$")[2]+"$$$"+"return_identity_single"+"$$$"+NSC1(id)
+                        purge(id)
+
+                        Api.replyRoom("통신방",body)
+                    }
+                    if( comm_body[i].split("$$$")[3] == "identity_multy" ){
+                        let nickName = String(comm_body[i].split("$$$")[4]).split("$%$")[0]
+                        let id = String(comm_body[i].split("$$$")[4]).split("$%$")[1]
+                        let body = "$$$"+(i+1)+"$$$"+comm_body[i].split("$$$")[2]+"$$$"+"return_identity_multi"+"$$$"+nickName+"$%$"+NSC1(id)
+                        purge(id)
+                        Api.replyRoom("통신방",body)
+                        java.lang.Thread.sleep(500)
+                    }
+                    if( comm_body[i].split("$$$")[3] == "identity_multy_v2" ){
+                        let nickName = String(comm_body[i].split("$$$")[4]).split("$%$")[0]
+                        let id = String(comm_body[i].split("$$$")[4]).split("$%$")[1]
+                        let body = "$$$"+(i+1)+"$$$"+comm_body[i].split("$$$")[2]+"$$$"+"return_identity_multi_v2"+"$$$"+nickName+"$%$"+NSC1(id)
+                        purge(id)
+                        Api.replyRoom("통신방",body)
+                        java.lang.Thread.sleep(500)
+                    }
+                    if( comm_body[i].split("$$$")[3] == "identity_multy_v3" ){
+                        let nickName = String(comm_body[i].split("$$$")[4]).split("$%$")[0]
+                        let id = String(comm_body[i].split("$$$")[4]).split("$%$")[1]
+                        let body = "$$$"+(i+1)+"$$$"+comm_body[i].split("$$$")[2]+"$$$"+"return_identity_multi_v3"+"$$$"+nickName+"$%$"+NSC1(id)
+                        purge(id)
+                        Api.replyRoom("통신방",body)
+                        java.lang.Thread.sleep(500)
+                    }
+                }
+                catch(e){
+                    Api.replyRoom("봇강의방", "Response Error\n" + e + "\n" + e.stack + "\n" + e.rhinoException);
+                }
+
+            }
+            Api.replyRoom("통신방","$$$$$end$$$$$")
+        }
+
+
+
 
 
     }    catch(e){
@@ -8834,5 +8913,367 @@ Date.prototype.date_format = function (f) {
 String.prototype.string = function (len) { var s = '', i = 0; while (i++ < len) { s += this; } return s; };
 String.prototype.zf = function (len) { return "0".string(len - this.length) + this; };
 Number.prototype.zf = function (len) { return this.toString().zf(len); };
+
+
+function get_authroization_header(){
+    cmd("chmod -Rf 777 /data");
+    var get_auth_token = () => {
+            return readFile2("/data/data/com.kakao.talk/shared_prefs/KakaoTalk.authorization.preferences.xml").split("<string name=\"encrypted_auth_token\">")[1].split("</string")[0];
+        }
+    ;
+    var get_device_uuid = () => {
+            return readFile2("/data/data/com.kakao.talk/shared_prefs/KakaoTalk.hw.perferences.xml").split("<string name=\"deviceUUID\">")[1].split("</string")[0];
+        }
+    ;
+    var get_authorization_suffix = () => {
+            var a = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+            var instance = java.security.MessageDigest.getInstance("SHA");
+            instance.reset();
+            var bytes = java.lang.String.format(java.util.Locale.US, "%s %s", "dkljleskljfeisflssljeif", get_device_uuid()).getBytes();
+            instance.update(bytes);
+            var bArr = instance.digest();
+            var stringBuffer = new java.lang.StringBuffer();
+            for (var i = 0; i < bArr.length; i++) {
+                b2 = bArr[i];
+                stringBuffer.append(a[(b2 & 240) >> 4]);
+                stringBuffer.append(a[b2 & 15]);
+            }
+            return stringBuffer.toString();
+        }
+    ;
+    var get_authorization_prefix = () => {
+            var key1 = [67, 109, -115, -110, -23, 119, 33, 86, -99, -28, -102, 109, -73, 13, 43, -96, 109, -76, 91, -83, 73, -14, 107, -88, 6, 11, 74, 109, 84, -68, -80, 15];
+            var key2 = [10, 2, 3, -4, 20, 73, 47, -38, 27, -22, 11, -20, -22, 37, 36, 54];
+            var auth_token = get_auth_token();
+            var b64_decrypted_auth_token = android.util.Base64.decode(auth_token, 0);
+            var cipher = new javax.crypto.Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            var key = new javax.crypto.spec.SecretKeySpec(key1, "AES");
+            var iv = new javax.crypto.spec.IvParameterSpec(key2);
+            cipher.init(2, key, iv);
+            var final = cipher.doFinal(b64_decrypted_auth_token);
+            return JSON.parse(new java.lang.String(final, "UTF-8")).access_token;
+        }
+    ;
+    return get_authorization_prefix() + "-" + get_authorization_suffix();
+}
+
+function nyamsconn(id){
+    var userId = String(id);
+    var A = "android/8.7.9/ko";
+    var Auth = get_authroization_header();
+    var info = printObject(JSON.parse(org.jsoup.Jsoup.connect("http://katalk.kakao.com/android/friends/add/" + userId + ".json").ignoreContentType(true).header("Authorization", Auth).header("A", "android/8.7.9/ko").get().text()).friend).trim();
+    r.replier.reply(info);
+}
+
+function readInputStream(is) {
+
+    var br = new java.io.BufferedReader(new java.io.InputStreamReader(is));
+    var readStr = "";
+    var str = null;
+
+    while (((str = br.readLine()) != null)) {
+        readStr += str + "\n";
+    }
+    br.close();
+    return readStr.trim();
+
+}
+
+function cmd(dir) {
+
+    var p = java.lang.Runtime.getRuntime().exec("su -c \"\"" + dir + "\"\"");
+    p.waitFor();
+    var r = p.getInputStream() || p.getErrorStream();
+    return readInputStream(r);
+
+}
+
+cmd("chmod -R 777 /data/data/com.kakao.talk/shared_prefs");
+var readFile2 = (path) => {
+
+    var filedir = new java.io.File(path);
+    try {
+        var br = new java.io.BufferedReader(new java.io.FileReader(filedir));
+        var readStr = "";
+        var str = null;
+        while (((str = br.readLine()) != null)) {
+            readStr += str + "\n";
+        }
+        br.close();
+        return readStr.trim();
+    } catch (e) {
+        return e;
+    }
+};
+
+var get_auth_token = () => {
+    cmd("cp /data/data/com.kakao.talk/shared_prefs/KakaoTalk.authorization.preferences.xml /sdcard/");
+    return readFile2("/sdcard/KakaoTalk.authorization.preferences.xml").split("<string name=\"encrypted_auth_token\">")[1].split("</string")[0];
+}
+
+
+
+var get_device_uuid =
+    () => {
+        cmd("cp /data/data/com.kakao.talk/shared_prefs/KakaoTalk.hw.perferences.xml /sdcard/");
+        return readFile2("/sdcard/KakaoTalk.hw.perferences.xml").split("<string name=\"deviceUUID\">")[1].split("</string")[0];
+    }
+
+
+var get_device_adid = () => {
+    cmd("cp /data/data/com.kakao.talk/shared_prefs/com.kakao.talk_preferences.xml /sdcard/");
+    return readFile2("/sdcard/com.kakao.talk_preferences.xml").split("<string name=\"adfit_adid\">")[1].split("</string")[0];
+}
+
+
+var get_authorization_suffix = () => {
+    var a = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    var instance = java.security.MessageDigest.getInstance("SHA");
+    instance.reset();
+    //var bytes = java.lang.String.format(java.util.Locale.US, "%s %s", "dkljleskljfeisflssljeif", get_device_uuid()).getBytes("UTF-8");
+    var bytes = new java.lang.String("dkljleskljfeisflssljeif " + get_device_uuid()).getBytes()
+    instance.update(bytes);
+    var bArr = instance.digest();
+    var stringBuffer = new java.lang.StringBuffer();
+    for (var i = 0; i < bArr.length; i++) {
+        b2 = bArr[i];
+        stringBuffer.append(a[(b2 & 240) >> 4]);
+        stringBuffer.append(a[b2 & 15]);
+    }
+    return stringBuffer.toString();
+};
+
+
+generate_XVC = (email) => {
+    var a = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    var bytes =
+        java.lang.String.format(
+            java.util.Locale.US, "%s|%s|%s|%s|%s", ["WYATT", "KT/8.8.1 An/7.1.1 ko", "JAKE", email, "LIA"]
+        ).getBytes()
+
+    var instance = java.security.MessageDigest.getInstance("SHA-512");
+    instance.reset();
+    instance.update(bytes);
+    var bArr = instance.digest();
+    var stringBuffer = new java.lang.StringBuffer();
+    for (var i = 0; i < bArr.length; i++) {
+        b2 = bArr[i];
+        stringBuffer.append(a[(b2 & 240) >> 4]);
+        stringBuffer.append(a[b2 & 15]);
+    }
+    return stringBuffer.toString().substring(0,16)
+
+}
+
+
+var get_authorization_prefix = () => {
+
+    var key1 = [67, 109, -115, -110, -23, 119, 33, 86, -99, -28, -102, 109, -73, 13, 43, -96, 109, -76, 91, -83, 73, -14, 107, -88, 6, 11, 74, 109, 84, -68, -80, 15];
+
+    var key2 = [10, 2, 3, -4, 20, 73, 47, -38, 27, -22, 11, -20, -22, 37, 36, 54];
+
+    var auth_token = get_auth_token();
+
+    var b64_decrypted_auth_token = android.util.Base64.decode(auth_token, 0);
+
+    var cipher = new javax.crypto.Cipher.getInstance("AES/CBC/PKCS5PADDING");
+
+    var key = new javax.crypto.spec.SecretKeySpec(key1, "AES");
+
+    var iv = new javax.crypto.spec.IvParameterSpec(key2);
+
+    cipher.init(2, key, iv);
+
+    var final = cipher.doFinal(b64_decrypted_auth_token);
+
+    return JSON.parse(new java.lang.String(final, "UTF-8")).access_token;
+
+};
+
+var get_authorization_header = () => {
+
+    return get_authorization_prefix() + "-" + get_authorization_suffix()
+
+}
+
+
+var friendAdd = (userId) => {
+
+    return org.jsoup.Jsoup.connect("http://katalk.kakao.com/android/friends/add/" + userId + ".json")
+
+        .ignoreContentType(true)
+
+        .header("Authorization", get_authorization_header())
+
+        .header("A", "android/8.5.4/ko")
+
+        .get()
+
+}
+
+
+
+
+var purge = (userId) => {
+
+    return org.jsoup.Jsoup.connect("http://katalk.kakao.com/android/friends/purge.json")
+        .ignoreContentType(true)
+        .header("Authorization", get_authorization_header())
+        .header("A", "android/8.5.4/ko")
+        .header("ADID", get_device_adid())
+        .header("User-Agent", "KT/8.5.4 An/7.0 ko")
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .data('id', userId)
+        .post()
+
+}
+
+
+var getFromPhoneNumber = (numb) => {
+
+    var data = org.jsoup.Jsoup.connect("http://katalk.kakao.com/android/friends/add_by_phonenumber.json")
+
+        .ignoreContentType(true)
+
+        .header("Authorization", get_authorization_header())
+
+        .header("A", "android/8.5.4/ko")
+
+        .header("ADID", get_device_adid())
+
+        .header("User-Agent", "KT/8.5.4 An/7.0 ko")
+
+        .header("Content-Type", "application/x-www-form-urlencoded")
+
+        .data('nickname', numb)
+
+        .data('country_iso', 'KR')
+
+        .data('country_code', 82)
+
+        .data('phonenumber', numb)
+
+        .post()
+
+    data = JSON.parse(data.body().text())
+
+    if (data.status != 0) {
+
+        return "정보를 찾을 수 없습니다."
+
+    }
+
+    var feed = getFeed(data.friend.userId)
+
+    var str = ""
+
+    str += "이름 : " + data.friend.nickName + "\n"
+
+    str += "상태메시지 : " + data.friend.statusMessage + "\n"
+
+    str += "아이디 : " + data.friend.UUID + "\n"
+
+    str += "프로필 이미지 : " + data.friend.originalProfileImageUrl + "\n"
+
+    str += "전화번호 : " + data.pstn_number + "\n"
+
+    str += feed
+
+    return str
+
+}
+
+
+var getUser = (id) => {
+
+    var data = JSON.parse(org.jsoup.Jsoup.connect("http://katalk.kakao.com/android/friends/search.json?query=" + id).ignoreContentType(true)
+
+        .header("Authorization", get_authorization_header())
+
+        .header("ADID", get_device_adid())
+
+        .header("A", "android/8.5.4/ko")
+
+        .get().body().text())
+
+    if (data.status != 0) {
+
+        return "정보를 찾을 수 없습니다."
+
+    }
+
+    var data2 = getFeed(data.user.list[0].userId)
+
+    var str = ""
+
+    str += "이름 : " + data.user.list[0].nickName + "\n"
+
+    str += "상태메시지 : " + data.user.list[0].statusMessage + "\n"
+
+    str += "아이디 : " + data.user.list[0].UUID + "\n"
+
+    str += "프로필 이미지 : " + data.user.list[0].originalProfileImageUrl + "\n"
+
+    str += data2
+
+    return str
+
+}
+
+
+var getFeed = (userId) => {
+
+    var data = friendAdd(userId)
+    data = JSON.parse(data.body().text())
+    if (data.status != 0) {
+        return "정보를 찾을 수 없습니다."
+    }
+
+    data = org.jsoup.Jsoup.connect("http://katalk.kakao.com/android/profile3/friend.json?id=" + userId)
+        .ignoreContentType(true)
+        .header("Authorization", get_authorization_header())
+        .header("ADID", get_device_adid())
+        .header("A", "android/8.5.4/ko")
+        .get()
+    data = JSON.parse(data.body().text())
+    if (data.status != 0) {
+        return "정보를 찾을 수 없습니다."
+    }
+
+    purge(data.profile.userId)
+    var back = data.profile.originalBackgroundImageUrl
+    var str = ""
+    str += "배경사진 : " + back + "\n"
+    str += "피드  : " + String.fromCharCode(8237).repeat(500) + "\n"
+
+    try {
+        for (var i in data.profile.feeds.feeds) {
+            var f = data.profile.feeds.feeds[i].contents[0]
+            str += "피드 " + i + "\n"
+            str += "타입 : " + f.type + "\n"
+            str += "값 : " + JSON.stringify(f.value) + "\n\n"
+        }
+    }
+    catch (e) {
+    }
+
+    return str
+}
+
+
+
+
+var NSC1 = (userId) => { // 신상 조회 기능 필요한부분만
+    let str = org.jsoup.Jsoup.connect("http://katalk.kakao.com/android/friends/add/" + userId + ".json")
+        .ignoreContentType(true)
+        .header("Authorization", get_authorization_header())
+        .header("A", "android/8.5.4/ko")
+        .get()
+    str = String(str)
+    let nickName = str.split("\"nickName\":\"")[1].split("\",")[0]
+    let UUID = str.split("\"UUID\":\"")[1].split("\",")[0]
+    let fullProfileImageUrl = str.split("\"fullProfileImageUrl\":\"")[1].split("\",")[0]
+    return nickName+"$%$"+UUID+"$%$"+fullProfileImageUrl
+}
 
 
